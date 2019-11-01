49d5946bac3f214df727787a98c2c33fe161553e
LexAV78 <gofride@gmail.com>
2019-11-01 23:50:21 +0200

    lesson_3

--- lesson_3/homework/1.js
+++ lesson_3/homework/1.js
@@ -20,21 +20,36 @@
  window.addEventListener('load', function(){
   var OurSliderImages = ['images/cat1.jpg', 'images/cat2.jpg', 'images/cat3.jpg', 'images/cat4.jpg', 'images/cat5.jpg', 'images/cat6.jpg', 'images/cat7.jpg', 'images/cat8.jpg'];
   var currentPosition = 0;
+  //
+  var elCheckBox=document.createElement("div");
+  var elCheck=document.createElement("input");
+  elCheck.type="checkbox";
+  elCheck.id="idCh";
+  elCheck.style.marginTop="20px";
+  elCheckBox.appendChild(elCheck);
+  var elCheckLabel = document.createElement('label')
+  elCheckLabel.htmlFor = "id";
+  elCheckLabel.appendChild(document.createTextNode('анимация'));
+  elCheckBox.appendChild(elCheckLabel);
+  document.getElementById("slider").appendChild(elCheckBox);
+  //
   var elImg = document.createElement("img"); 
   elImg.style.transition="1s"; 
   document.getElementById("slider").appendChild(elImg);
+  //
   function RenderImage(){
     if(currentPosition<0)currentPosition=OurSliderImages.length-1;
     if(OurSliderImages.length==currentPosition)currentPosition=0;
     elImg.setAttribute("src",OurSliderImages[currentPosition]); 
   }  
   function btnClick(vItem){
-    elImg.style.opacity="0";
     if(vItem.target.id=="NextSilde")currentPosition++;else currentPosition--;
-    setTimeout( function(){
+    if(idCh.checked){
+      elImg.style.opacity="0";
+      setTimeout( function(){
       RenderImage();
       elImg.style.opacity="1";
-    }, 1000);
+    }, 1000);}else  RenderImage();
   };
   document.querySelectorAll("button").forEach(vItem=>vItem.addEventListener("click",btnClick));
   //
